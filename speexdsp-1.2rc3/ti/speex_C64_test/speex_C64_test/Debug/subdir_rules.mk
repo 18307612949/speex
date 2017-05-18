825930c78eedd00e775023d971d3151dfbbbe5ab
zzh <1814943084@qq.com>
2017-05-18 14:15:37 +0800

    initial commit

---
+++ speexdsp-1.2rc3/ti/speex_C64_test/speex_C64_test/Debug/subdir_rules.mk
@@ -0,0 +1,83 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Each subdirectory must supply rules for building sources it contributes
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug/buffer.obj: C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/buffer.c $(GEN_OPTS) $(GEN_SRCS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C6000 Compiler'
+	"C:/ti/ccsv5/tools/compiler/c6000_7.3.4/bin/cl6x" -mv6400 -g -O3 --define="_DEBUG" --define="CONFIG_TI_C6X" --define="HAVE_CONFIG_H" --include_path="C:/ti/ccsv5/tools/compiler/c6000_7.3.4/include" --include_path="C:/ti/xdais_7_21_01_07/packages/ti/xdais" --include_path="c:/CCSTUD~1.3PL/C6000/csl/include" --include_path="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/ti" --include_path="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/include" --display_error_number --abi=coffabi --obj_directory="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug" --preproc_with_compile --preproc_dependency="buffer.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug/fftwrap.obj: C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/fftwrap.c $(GEN_OPTS) $(GEN_SRCS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C6000 Compiler'
+	"C:/ti/ccsv5/tools/compiler/c6000_7.3.4/bin/cl6x" -mv6400 -g -O3 --define="_DEBUG" --define="CONFIG_TI_C6X" --define="HAVE_CONFIG_H" --include_path="C:/ti/ccsv5/tools/compiler/c6000_7.3.4/include" --include_path="C:/ti/xdais_7_21_01_07/packages/ti/xdais" --include_path="c:/CCSTUD~1.3PL/C6000/csl/include" --include_path="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/ti" --include_path="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/include" --display_error_number --abi=coffabi --obj_directory="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug" --preproc_with_compile --preproc_dependency="fftwrap.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug/filterbank.obj: C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/filterbank.c $(GEN_OPTS) $(GEN_SRCS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C6000 Compiler'
+	"C:/ti/ccsv5/tools/compiler/c6000_7.3.4/bin/cl6x" -mv6400 -g -O3 --define="_DEBUG" --define="CONFIG_TI_C6X" --define="HAVE_CONFIG_H" --include_path="C:/ti/ccsv5/tools/compiler/c6000_7.3.4/include" --include_path="C:/ti/xdais_7_21_01_07/packages/ti/xdais" --include_path="c:/CCSTUD~1.3PL/C6000/csl/include" --include_path="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/ti" --include_path="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/include" --display_error_number --abi=coffabi --obj_directory="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug" --preproc_with_compile --preproc_dependency="filterbank.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug/jitter.obj: C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/jitter.c $(GEN_OPTS) $(GEN_SRCS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C6000 Compiler'
+	"C:/ti/ccsv5/tools/compiler/c6000_7.3.4/bin/cl6x" -mv6400 -g -O3 --define="_DEBUG" --define="CONFIG_TI_C6X" --define="HAVE_CONFIG_H" --include_path="C:/ti/ccsv5/tools/compiler/c6000_7.3.4/include" --include_path="C:/ti/xdais_7_21_01_07/packages/ti/xdais" --include_path="c:/CCSTUD~1.3PL/C6000/csl/include" --include_path="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/ti" --include_path="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/include" --display_error_number --abi=coffabi --obj_directory="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug" --preproc_with_compile --preproc_dependency="jitter.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug/kiss_fft.obj: C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/kiss_fft.c $(GEN_OPTS) $(GEN_SRCS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C6000 Compiler'
+	"C:/ti/ccsv5/tools/compiler/c6000_7.3.4/bin/cl6x" -mv6400 -g -O3 --define="_DEBUG" --define="CONFIG_TI_C6X" --define="HAVE_CONFIG_H" --include_path="C:/ti/ccsv5/tools/compiler/c6000_7.3.4/include" --include_path="C:/ti/xdais_7_21_01_07/packages/ti/xdais" --include_path="c:/CCSTUD~1.3PL/C6000/csl/include" --include_path="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/ti" --include_path="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/include" --display_error_number --abi=coffabi --obj_directory="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug" --preproc_with_compile --preproc_dependency="kiss_fft.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug/kiss_fftr.obj: C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/kiss_fftr.c $(GEN_OPTS) $(GEN_SRCS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C6000 Compiler'
+	"C:/ti/ccsv5/tools/compiler/c6000_7.3.4/bin/cl6x" -mv6400 -g -O3 --define="_DEBUG" --define="CONFIG_TI_C6X" --define="HAVE_CONFIG_H" --include_path="C:/ti/ccsv5/tools/compiler/c6000_7.3.4/include" --include_path="C:/ti/xdais_7_21_01_07/packages/ti/xdais" --include_path="c:/CCSTUD~1.3PL/C6000/csl/include" --include_path="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/ti" --include_path="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/include" --display_error_number --abi=coffabi --obj_directory="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug" --preproc_with_compile --preproc_dependency="kiss_fftr.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug/mdf.obj: C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/mdf.c $(GEN_OPTS) $(GEN_SRCS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C6000 Compiler'
+	"C:/ti/ccsv5/tools/compiler/c6000_7.3.4/bin/cl6x" -mv6400 -g -O3 --define="_DEBUG" --define="CONFIG_TI_C6X" --define="HAVE_CONFIG_H" --include_path="C:/ti/ccsv5/tools/compiler/c6000_7.3.4/include" --include_path="C:/ti/xdais_7_21_01_07/packages/ti/xdais" --include_path="c:/CCSTUD~1.3PL/C6000/csl/include" --include_path="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/ti" --include_path="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/include" --display_error_number --abi=coffabi --obj_directory="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug" --preproc_with_compile --preproc_dependency="mdf.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug/preprocess.obj: C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/preprocess.c $(GEN_OPTS) $(GEN_SRCS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C6000 Compiler'
+	"C:/ti/ccsv5/tools/compiler/c6000_7.3.4/bin/cl6x" -mv6400 -g -O3 --define="_DEBUG" --define="CONFIG_TI_C6X" --define="HAVE_CONFIG_H" --include_path="C:/ti/ccsv5/tools/compiler/c6000_7.3.4/include" --include_path="C:/ti/xdais_7_21_01_07/packages/ti/xdais" --include_path="c:/CCSTUD~1.3PL/C6000/csl/include" --include_path="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/ti" --include_path="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/include" --display_error_number --abi=coffabi --obj_directory="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug" --preproc_with_compile --preproc_dependency="preprocess.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug/resample.obj: C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/resample.c $(GEN_OPTS) $(GEN_SRCS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C6000 Compiler'
+	"C:/ti/ccsv5/tools/compiler/c6000_7.3.4/bin/cl6x" -mv6400 -g -O3 --define="_DEBUG" --define="CONFIG_TI_C6X" --define="HAVE_CONFIG_H" --include_path="C:/ti/ccsv5/tools/compiler/c6000_7.3.4/include" --include_path="C:/ti/xdais_7_21_01_07/packages/ti/xdais" --include_path="c:/CCSTUD~1.3PL/C6000/csl/include" --include_path="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/ti" --include_path="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/include" --display_error_number --abi=coffabi --obj_directory="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug" --preproc_with_compile --preproc_dependency="resample.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug/scal.obj: C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/scal.c $(GEN_OPTS) $(GEN_SRCS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C6000 Compiler'
+	"C:/ti/ccsv5/tools/compiler/c6000_7.3.4/bin/cl6x" -mv6400 -g -O3 --define="_DEBUG" --define="CONFIG_TI_C6X" --define="HAVE_CONFIG_H" --include_path="C:/ti/ccsv5/tools/compiler/c6000_7.3.4/include" --include_path="C:/ti/xdais_7_21_01_07/packages/ti/xdais" --include_path="c:/CCSTUD~1.3PL/C6000/csl/include" --include_path="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/ti" --include_path="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/include" --display_error_number --abi=coffabi --obj_directory="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug" --preproc_with_compile --preproc_dependency="scal.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug/testdenoise.obj: ../testdenoise.c $(GEN_OPTS) $(GEN_SRCS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C6000 Compiler'
+	"C:/ti/ccsv5/tools/compiler/c6000_7.3.4/bin/cl6x" -mv6400 -g -O3 --define="_DEBUG" --define="CONFIG_TI_C6X" --define="HAVE_CONFIG_H" --include_path="C:/ti/ccsv5/tools/compiler/c6000_7.3.4/include" --include_path="C:/ti/xdais_7_21_01_07/packages/ti/xdais" --include_path="c:/CCSTUD~1.3PL/C6000/csl/include" --include_path="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/ti" --include_path="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/include" --display_error_number --abi=coffabi --obj_directory="C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug" --preproc_with_compile --preproc_dependency="testdenoise.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ speexdsp-1.2rc3/ti/speex_C64_test/speex_C64_test/Debug/subdir_vars.mk
@@ -0,0 +1,90 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/speex_C64_test.cmd 
+
+LIB_SRCS += \
+C:/ti/ccsv5/tools/compiler/c6000_7.3.4/lib/rts6400.lib 
+
+C_SRCS += \
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/buffer.c \
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/fftwrap.c \
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/filterbank.c \
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/jitter.c \
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/kiss_fft.c \
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/kiss_fftr.c \
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/mdf.c \
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/preprocess.c \
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/resample.c \
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/scal.c \
+../testdenoise.c 
+
+OBJS += \
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug/buffer.obj \
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug/fftwrap.obj \
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug/filterbank.obj \
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug/jitter.obj \
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug/kiss_fft.obj \
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug/kiss_fftr.obj \
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug/mdf.obj \
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug/preprocess.obj \
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug/resample.obj \
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug/scal.obj \
+C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/ti/speex_C64_test/Debug/testdenoise.obj 
+
+C_DEPS += \
+./buffer.pp \
+./fftwrap.pp \
+./filterbank.pp \
+./jitter.pp \
+./kiss_fft.pp \
+./kiss_fftr.pp \
+./mdf.pp \
+./preprocess.pp \
+./resample.pp \
+./scal.pp \
+./testdenoise.pp 
+
+C_DEPS__QUOTED += \
+"buffer.pp" \
+"fftwrap.pp" \
+"filterbank.pp" \
+"jitter.pp" \
+"kiss_fft.pp" \
+"kiss_fftr.pp" \
+"mdf.pp" \
+"preprocess.pp" \
+"resample.pp" \
+"scal.pp" \
+"testdenoise.pp" 
+
+OBJS__QUOTED += \
+"C:\电机驱动\麦克风阵列文档\麦克风阵列文档\Matlab文件\speex\speexdsp-1.2rc3\ti\speex_C64_test\Debug\buffer.obj" \
+"C:\电机驱动\麦克风阵列文档\麦克风阵列文档\Matlab文件\speex\speexdsp-1.2rc3\ti\speex_C64_test\Debug\fftwrap.obj" \
+"C:\电机驱动\麦克风阵列文档\麦克风阵列文档\Matlab文件\speex\speexdsp-1.2rc3\ti\speex_C64_test\Debug\filterbank.obj" \
+"C:\电机驱动\麦克风阵列文档\麦克风阵列文档\Matlab文件\speex\speexdsp-1.2rc3\ti\speex_C64_test\Debug\jitter.obj" \
+"C:\电机驱动\麦克风阵列文档\麦克风阵列文档\Matlab文件\speex\speexdsp-1.2rc3\ti\speex_C64_test\Debug\kiss_fft.obj" \
+"C:\电机驱动\麦克风阵列文档\麦克风阵列文档\Matlab文件\speex\speexdsp-1.2rc3\ti\speex_C64_test\Debug\kiss_fftr.obj" \
+"C:\电机驱动\麦克风阵列文档\麦克风阵列文档\Matlab文件\speex\speexdsp-1.2rc3\ti\speex_C64_test\Debug\mdf.obj" \
+"C:\电机驱动\麦克风阵列文档\麦克风阵列文档\Matlab文件\speex\speexdsp-1.2rc3\ti\speex_C64_test\Debug\preprocess.obj" \
+"C:\电机驱动\麦克风阵列文档\麦克风阵列文档\Matlab文件\speex\speexdsp-1.2rc3\ti\speex_C64_test\Debug\resample.obj" \
+"C:\电机驱动\麦克风阵列文档\麦克风阵列文档\Matlab文件\speex\speexdsp-1.2rc3\ti\speex_C64_test\Debug\scal.obj" \
+"C:\电机驱动\麦克风阵列文档\麦克风阵列文档\Matlab文件\speex\speexdsp-1.2rc3\ti\speex_C64_test\Debug\testdenoise.obj" 
+
+C_SRCS__QUOTED += \
+"C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/buffer.c" \
+"C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/fftwrap.c" \
+"C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/filterbank.c" \
+"C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/jitter.c" \
+"C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/kiss_fft.c" \
+"C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/kiss_fftr.c" \
+"C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/mdf.c" \
+"C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/preprocess.c" \
+"C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/resample.c" \
+"C:/电机驱动/麦克风阵列文档/麦克风阵列文档/Matlab文件/speex/speexdsp-1.2rc3/libspeexdsp/scal.c" \
+"../testdenoise.c" 
+
+
